41d3d3c01c954337a6ba091388c8d3bb30622589
Daniel Spielman <daniel.spielman@yale.edu>
2020-09-11 19:52:20 -0400

    fixing docstrings, changing option names, fixing n_starts

--- src/try_many.jl
+++ src/try_many.jl
@@ -1,28 +1,32 @@
 
 """
-    x, val = try_many(obj::Function, gen::Function, sense::Symbol; n = Inf, t_lim = Inf, ...
+    x, val = try_many(obj::Function, gen::Function, sense::Symbol; n_tries = Inf, t_lim = Inf, ...
 
 Return the best of many calls of `obj` on the output of `gen`,
 where best is according to `sense` which should be `:Max` or `:Min`.
-Repeats for `n` trials or until `t_lim` seconds pass.  One of these must be set.
+Repeats for `n_tries` trials or until `t_lim` seconds pass.  One of these must be set.
 
 # Arguments
+These are required:
 - `obj::Function`: The objective function
 - `gen::Function`: A zero-argument function. `gen()` should generate an input to `obj`.
    For example, `()->rand(4)`.
 - `sense::Symbol`: `:Min` or `:Max`
-- `n::Integer`: An upper bound on the number of inputs to try.
+At least one of the following two must be set:
+- `n_tries::Integer`: An upper bound on the number of inputs to try.
 - `t_lim`: Will stop after `t_lim` seconds.
-- `fn_base`: if this is non-empty, then output will be saved to `\$(fn_base).txt` and `\$(fn_base).jld`.
+The rest are optional:
+- `stop_val`: will stop as soon as find a value better than this
+- `file_base`: if this is non-empty, then output will be saved to `\$(file_base).txt` and `\$(file_base).jld`.
 - `verbosity::Integer`: if 0, should be no output.
     1, will output just the best so far each time a new one is found.
     2, will output everything.
 - `seed::Bool`: if negative, nothing happens.  Otherwise, seed the ith trial with the ith random seed.
 - `local_rng::Bool`: if true, gen() should take a RNG as input, and it will be seeded as suggested by `seed`.
-  This prevents the gen erator for instances from intefering with any generator the algorithm might use.
+  This prevents the generator for instances from intefering with any generator the algorithm might use.
 - `verbose::Bool`: for backwards compatibility.  Used to set `verbosity`.
 - `par_batch::Integer`: if 0, don't parallelize. If > 0, run in batches of this size.
-	Will force seed, unless using local_rng.  local_rng parallelizes the time of generation.
+    Will force seed, unless using `local_rng`.  `local_rng` parallelizes the time of generation.
 """
 function try_many(func::Function, gen::Function, sense::Symbol; kwargs...)
 
@@ -34,16 +38,17 @@ end
 """
 Like try_many, but func returns (x, val), where x has been transformed.
 """
-function try_many_trans(func::Function, gen::Function, sense::Symbol; n = Inf, t_lim = Inf,
-    fn_base = "",
+function try_many_trans(func::Function, gen::Function, sense::Symbol; n_tries = Inf, t_lim = Inf,
+    file_base = "",
     verbose = false,
     verbosity = 1 + verbose,
     seed = false,
     local_rng = false,
-	par_batch = 0)
+    par_batch = 0,
+    stop_val = sense == :Max ? Inf : -Inf)
 
     @assert sense == :Max || sense == :Min
-    @assert n < Inf || t_lim < Inf
+    @assert n_tries < Inf || t_lim < Inf
     @assert !local_rng || seed
 
     if sense == :Max
@@ -59,14 +64,14 @@ function try_many_trans(func::Function, gen::Function, sense::Symbol; n = Inf, t
     bestx = []
     bests = []
 
-    if !isempty(fn_base)
-        txt_file = "$(fn_base).txt"
-        jld_file = "$(fn_base).jld"
+    if !isempty(file_base)
+        txt_file = "$(file_base).txt"
+        jld_file = "$(file_base).jld"
     else
         txt_file, jld_file = "", ""
     end
 
-    !isempty(fn_base) && verbosity > 0 &&
+    !isempty(file_base) && verbosity > 0 &&
         println("writing to $(txt_file) and $(jld_file)")
 
     info_to_file(txt_file)
@@ -79,7 +84,7 @@ function try_many_trans(func::Function, gen::Function, sense::Symbol; n = Inf, t
     t0 = time()
     i = 1
 
-    while (time()-t0 < t_lim && i < n)
+    while (time()-t0 < t_lim && i < n_tries && comp(stop_val, bestval))
 
 		if par_batch == 0
 	        if local_rng
@@ -108,7 +113,7 @@ function try_many_trans(func::Function, gen::Function, sense::Symbol; n = Inf, t
             if verbosity > 0
 		        report(i, val, bestx, txt_file)
             end
-            !isempty(fn_base) && save(jld_file, "bests", bests)
+            !isempty(file_base) && save(jld_file, "bests", bests)
         elseif verbosity > 1
             println("iteration: $(i), val: $(val)")
         end
